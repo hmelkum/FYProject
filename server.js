ee576f8aaa5134fbb019cc2de759390ff0e87e50
Hayk Melkumyan <hayk.melkumyan@tumo.org>
2020-03-06 20:46:45 +0000

    Heroku H10 error solution test 3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const exec = require('child_process').exec;
 
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
