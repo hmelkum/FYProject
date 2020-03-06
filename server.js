5dd7adccb3a3e7c2da299a9383f7b5502773321b
Hayk Melkumyan <hayk.melkumyan@tumo.org>
2020-03-06 20:25:05 +0000

    Heroku error solution test 1

--- server.js
+++ server.js
@@ -14,37 +14,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: __dirname})
 });
-// app.get('/Home', (req, res) => {
-//     res.redirect('/');
-// });
-// app.get('/HTML', (req, res) => {
-//     res.sendFile('/public/html/HTML.html', {root: __dirname})
-// });
-// app.get('/LaTeX', (req, res) => {
-//     res.sendFile('/public/html/LaTeX.html', {root: __dirname})
-// });
-// app.get('/TeamPage', (req, res) => {
-//     res.sendFile('/public/html/TeamPage.html', {root: __dirname})
-// });
+app.get('/Home', (req, res) => {
+    res.redirect('/');
+});
+app.get('/HTML', (req, res) => {
+    res.sendFile('/public/html/HTML.html', {root: __dirname})
+});
+app.get('/LaTeX', (req, res) => {
+    res.sendFile('/public/html/LaTeX.html', {root: __dirname})
+});
+app.get('/TeamPage', (req, res) => {
+    res.sendFile('/public/html/TeamPage.html', {root: __dirname})
+});
 
-// app.post('/api/update/latex.pdf',(req,res)=>{
-//     // data = req.body.text;
-//     // let i = 0;
+app.post('/api/update/latex.pdf',(req,res)=>{
+    // data = req.body.text;
+    // let i = 0;
 
-//     // for(let i=0; i< data.length; i++)
-//     // {
-//     //     queue(i,data);
-//     //     exec("echo off", (error, stdOut, stdErr) =>{});
-//     // }
+    // for(let i=0; i< data.length; i++)
+    // {
+    //     queue(i,data);
+    //     exec("echo off", (error, stdOut, stdErr) =>{});
+    // }
         
-//     // function queue(i,data)
-//     // {
-//     //     let command = `echo ${data[i]} >> public/src/doc/latex.txt`;
-//     //     exec(command, function (error, stdOut, stdErr) {
-//     //     console.log(command);
-//     //     console.log("DONE: "+i);
-//     //     });
-//     // }
-// });
+    // function queue(i,data)
+    // {
+    //     let command = `echo ${data[i]} >> public/src/doc/latex.txt`;
+    //     exec(command, function (error, stdOut, stdErr) {
+    //     console.log(command);
+    //     console.log("DONE: "+i);
+    //     });
+    // }
+});
 
 app.listen(port, () => console.log(`Listening to port : ${port}`));
