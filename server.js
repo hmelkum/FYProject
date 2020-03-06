0be1c89fcdd09abf8d0776071ff99b1514eba93b
Hayk Melkumyan <hayk.melkumyan@tumo.org>
2020-03-06 20:19:46 +0000

    Working on Heroku R10 error

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const exec = require('child_process').exec;
 
 const app = express();
 
+const port = process.env.port || 3000;
+
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
@@ -12,38 +14,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: __dirname})
 });
-app.get('/Home', (req, res) => {
-    res.redirect('/');
-});
-app.get('/HTML', (req, res) => {
-    res.sendFile('/public/html/HTML.html', {root: __dirname})
-});
-app.get('/LaTeX', (req, res) => {
-    res.sendFile('/public/html/LaTeX.html', {root: __dirname})
-});
-app.get('/TeamPage', (req, res) => {
-    res.sendFile('/public/html/TeamPage.html', {root: __dirname})
-});
+// app.get('/Home', (req, res) => {
+//     res.redirect('/');
+// });
+// app.get('/HTML', (req, res) => {
+//     res.sendFile('/public/html/HTML.html', {root: __dirname})
+// });
+// app.get('/LaTeX', (req, res) => {
+//     res.sendFile('/public/html/LaTeX.html', {root: __dirname})
+// });
+// app.get('/TeamPage', (req, res) => {
+//     res.sendFile('/public/html/TeamPage.html', {root: __dirname})
+// });
 
-app.post('/api/update/latex.pdf',(req,res)=>{
-    // data = req.body.text;
-    // let i = 0;
+// app.post('/api/update/latex.pdf',(req,res)=>{
+//     // data = req.body.text;
+//     // let i = 0;
 
-    // for(let i=0; i< data.length; i++)
-    // {
-    //     queue(i,data);
-    //     exec("echo off", (error, stdOut, stdErr) =>{});
-    // }
+//     // for(let i=0; i< data.length; i++)
+//     // {
+//     //     queue(i,data);
+//     //     exec("echo off", (error, stdOut, stdErr) =>{});
+//     // }
         
-    // function queue(i,data)
-    // {
-    //     let command = `echo ${data[i]} >> public/src/doc/latex.txt`;
-    //     exec(command, function (error, stdOut, stdErr) {
-    //     console.log(command);
-    //     console.log("DONE: "+i);
-    //     });
-    // }
-});
+//     // function queue(i,data)
+//     // {
+//     //     let command = `echo ${data[i]} >> public/src/doc/latex.txt`;
+//     //     exec(command, function (error, stdOut, stdErr) {
+//     //     console.log(command);
+//     //     console.log("DONE: "+i);
+//     //     });
+//     // }
+// });
 
-const port = process.env.port || 3000;
 app.listen(port, () => console.log(`Listening to port : ${port}`));
